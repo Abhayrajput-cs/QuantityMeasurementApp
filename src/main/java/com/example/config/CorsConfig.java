203cda65c8e1393a4f909392930d2448ec867004
Abhay Pratap singh <122587173+Abhayrajput-cs@users.noreply.github.com>
2026-04-16 22:34:19 +0530

    Update CorsConfig.java

--- src/main/java/com/example/config/CorsConfig.java
+++ src/main/java/com/example/config/CorsConfig.java
@@ -1,9 +1,9 @@
-// package com.example.config;
+package com.example.config;
 
-// import org.springframework.context.annotation.Bean;
-// import org.springframework.context.annotation.Configuration;
-// import org.springframework.web.servlet.config.annotation.CorsRegistry;
-// import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 // @Configuration
 // public class CorsConfig {
