13b6140c8f0eefb88e2a16bc46545a86212b3798
Abhay Pratap singh <122587173+Abhayrajput-cs@users.noreply.github.com>
2026-04-16 22:38:27 +0530

    Update CorsConfig.java

--- src/main/java/com/example/config/CorsConfig.java
+++ src/main/java/com/example/config/CorsConfig.java
@@ -5,37 +5,18 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-// @Configuration
-// public class CorsConfig {
-
-//     @Bean
-//     public WebMvcConfigurer corsConfigurer() {
-//         return new WebMvcConfigurer() {
-//             @Override
-//             public void addCorsMappings(CorsRegistry registry) {
-//                 registry.addMapping("/**")
-//                         .allowedOrigins("http://35.172.201.210")
-//                         .allowedMethods("*")
-//                         .allowedHeaders("*")
-//                         .allowCredentials(true);
-//             }
-//         };
-//     }
-// }
 @Configuration
 public class CorsConfig {
 
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
+
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins(
-                                "http://abhayqma.duckdns.org",
-                                "http://abhayqma.duckdns.org:8080"
-                        )
-                        .allowedMethods("*")
+                        .allowedOrigins("http://abhayqma.duckdns.org")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
             }
